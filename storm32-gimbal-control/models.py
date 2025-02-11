01522f66903c2065a730c5e7221207c92aeef69e
al1can <ac.gunduz7878@gmail.com>
2025-02-11 11:46:51 +0300

    added standby, docamera, scriptcontrol modes

--- storm32-gimbal-control/models.py
+++ storm32-gimbal-control/models.py
@@ -21,3 +21,22 @@ class PanMode(Enum):
     PAN_HOLD_HOLD = 4
     PAN_HOLD_PAN = 5
     HOLD_PAN_PAN = 6
+
+class StandBySwitch(Enum):
+    OFF = 0
+    ON = 1
+    
+class DoCameraMode(Enum):
+    OFF = 0
+    IRSHUTTER = 1
+    IRSHUTTERDELAYED = 2
+    IRVIDEOON = 3
+    IRVIDEOOFF = 4
+    
+class ScriptControlMode(Enum):
+    OFF = 0
+    CASE_DEFAULT = 1
+    CASE_1 = 2
+    CASE_2 = 3
+    CASE_3 = 4
+    
