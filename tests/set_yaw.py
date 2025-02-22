4d70637325a4fc25406d5c88251108603fbe8fdb
al1can <ac.gunduz7878@gmail.com>
2025-02-22 13:34:15 +0300

    added set yaw test

---
+++ tests/set_yaw.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import serial
+import threading
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from storm32_gimbal_control import core
+from storm32_gimbal_control import utils
+
+serial_port = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+
+thread = threading.Thread(target=utils.read_from_serial, args=(serial_port,))
+thread.start()
+
+core.set_yaw(serial_port, 0)
